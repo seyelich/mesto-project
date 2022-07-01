dcb824d01b43bbdec5c88b0ad5401cf9b3d930ca
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2022-07-01 14:06:39 +0500

    start Card class

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,39 @@
 import { openPopup } from "./utils";
-import { popupPhoto, cardsContainer, popupPhotoImg, popupPhotoTitle } from "./constants";
+import { popupPhoto, cardsContainer, popupPhotoImg, popupPhotoTitle, cardLink, cardName } from "./constants";
 import { deleteCard, deleteLike, setLike,checkResult } from "./api";
+
+export class Card {
+  constructor (cardNameValue, cardLinkValue, cardLikes, userId, cardId, template) {
+    this._title = cardNameValue;
+    this._src = cardLinkValue;
+    this._likes = cardLikes;
+    this._id = cardId;
+    this._ownerId = userId;
+    this._template = template;
+
+    // Темплат пока не передаётся в объект, поэтому делаем поиск элемента по-старинке
+    this._element = document.querySelector('#card-template').cloneNode.firstElementChild;
+    this._image = this._element.querySelector('.cardImg');
+    this._titleElem = this._element.querySelector('card__title');
+    this._deleteButton = this._element.querySelector('.card__delete-button');
+    this._likeButton = this._element.querySelector('.card__like-button');
+    this._likeCounter = this._element.querySelector('.card__like-counter');
+    
+    this._isOwned = this._ownerId === '345b79856fbd9fc381d12a7d'; // WTF?
+  }
+  
+  _initLayout () {
+    this._image.src = this._scr;
+    this._image.alt = this._title;
+    this._titleElem.textContent = this._title;
+    this._likeCounter.textContent = this._likes.length;
+  }
+
+  create () {
+    this._initLayout();
+  }
+}
+
 
 export function createCard(cardNameValue, cardLinkValue, cardLikes, userId, cardId) {
   const cardTemplate = document.querySelector('#card-template').content;
